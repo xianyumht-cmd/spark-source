19e022dc2b5e59009543fcf089c2f2a5386e41fd
xianyumht-cmd <xianyumht@gmail.com>
2026-08-05 23:23:06 +0800

    添加磁盘入口反汇编证据

---
+++ data/entrypoint-disasm.asm
@@ -0,0 +1,32 @@
+; PE32 disk entry excerpt
+; ImageBase = 0x00400000
+; Entry RVA = 0x03483820
+
+03883820: e9 9b a1 0d 02    jmp 0x0595d9c0
+03883825: cc                int3
+03883826: cc                int3
+03883827: cc                int3
+03883828: cc                int3
+03883829: cc                int3
+0388382a: cc                int3
+0388382b: cc                int3
+0388382c: cc                int3
+0388382d: cc                int3
+0388382e: cc                int3
+0388382f: cc                int3
+03883830: cc                int3
+03883831: cc                int3
+03883832: cc                int3
+03883833: cc                int3
+03883834: cc                int3
+03883835: cc                int3
+03883836: cc                int3
+03883837: cc                int3
+03883838: cc                int3
+03883839: cc                int3
+0388383a: cc                int3
+0388383b: cc                int3
+0388383c: cc                int3
+0388383d: cc                int3
+0388383e: cc                int3
+0388383f: cc                int3
